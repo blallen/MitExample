40acef6901c64f3b47cb075dfa2349f3533a2c48
Brandon <ballen@mit.edu>
2015-06-09 09:13:24 -0400

    Can calculate electron efficiencygit commit -m 'Can calculate electron efficiency!! Integrate pdf's over Z peak window and then takes ratio.'  Integrate pdf's over Z peak window and then takes ratio.

--- Fitting/scripts/example.py
+++ Fitting/scripts/example.py
@@ -5,11 +5,21 @@ ROOT.gSystem.Load('libRooFit.so')
 ROOT.gSystem.Load('libMitExampleDataFormats.so')
 ROOT.gSystem.Load('libMitExampleFitting.so')
 
+RooFit = ROOT.RooFit
+mithep = ROOT.mithep
 
 def fitZpeak( mass, name, dataset ):
+    '''Fit mass peak with BreitWigner signal plus Exponential exponential background and extract number of electrons within Z window [80,100]'''
+
+    print 'Fitting for '+name
+    
     sig = workspace.factory('BreitWigner::sig_'+name+'(mass_'+name+', m0_'+name+'[90.,50.,130.], w_'+name+'[1.0,0.,5.0])')
     bkg = workspace.factory('Exponential::bkg_'+name+'(mass_'+name+', lambda_'+name+'[-1.,-100.,0.])')
-    spb = workspace.factory('SUM::spb_'+name+'(nsig_'+name+'[200.,0.,10000.] * sig_'+name+',nbkg_'+name+'[200.,0.,1000.] * bkg_'+name+')')
+
+    nsig = workspace.factory('nsig_'+name+'[200.,0.,10000.]')
+    nbkg = workspace.factory('nbkg_'+name+'[200.,0.,10000.]')
+    
+    spb = workspace.factory('SUM::spb_'+name+'(nsig_'+name+' * sig_'+name+',nbkg_'+name+' * bkg_'+name+')')
 
     spb.fitTo(dataset)
 
@@ -21,20 +31,19 @@ def fitZpeak( mass, name, dataset ):
     frame.Draw()
     canvas.SaveAs('DiEleMass_'+name+'.png')
 
-    return spb
-    
-    # bad integral code
-    '''
-    mass.setRange('Zpeak', 80., 100.)
+    mass.setRange('Zpeak_'+name, 80., 100.)
+    mass.setRange('all_'+name,0.,200.)
     tmp = ROOT.RooArgSet(mass)
-    print mass
-    nReal = spb.createIntegral( mass, mass, RooFit.Range('Zpeak'))
+    fReal = sig.createIntegral( tmp, 'Zpeak_'+name).getVal() / sig.createIntegral( tmp, 'all_'+name).getVal()
+    nReal = fReal * ( nsig.getVal())
     
-    return nReal
-    '''
+    print 'Total number of events is '+str(nsig.getVal())
+    print 'Fraction on Z peak is '+str(fReal)
+    print 'Number of Events on Z peak is '+str(nReal)    
 
-RooFit = ROOT.RooFit
-mithep = ROOT.mithep
+    return nReal
+
+###
 
 source = ROOT.TFile.Open('ntuples_noid_skim.root')
 tree = source.Get('events')
@@ -47,44 +56,11 @@ argset = ROOT.RooArgSet(mass_noID, mass_passID)
 
 dataset = mithep.FittingUtils.createDataSet(tree, argset)
 
-spb_noID = fitZpeak( mass_noID, 'noID', dataset)
-spb_passID = fitZpeak( mass_passID, 'passID', dataset)
-
-'''
 nNoID = fitZpeak( mass_noID, 'noID', dataset)
 nPassID = fitZpeak( mass_passID, 'passID', dataset)
 
 eff = nPassID / nNoID
 
 print 'Electron Efficiency is '+str(eff)
-'''
 
 sys.stdin.readline() # let the script hang around
-
-
-'''
-output = ROOT.TFile.Open('TnPlots_noid.root', 'recreate')
-
-hDiEleMass = ROOT.TH1F('hDiEleMass', 'M_{ee}', 1000, 0., 200.)
-
-for event in tree :
-    for iP in xrange(event.nPairs) :
-        tag = mithep.Tag(pair.first)
-        probe = mithep.DataFormats.TnPEvent.Probe(pair.second)
-
-        if tag.charge == probe.charge : continue
-
-        pTag = ROOT.TLorentzVector()
-        pProbe = ROOT.TLorentzVector()
-        pTag.SetPtEtaPhiE(tag.pt, tag.eta, tag.phi, tag.energy)
-        pProbe.SetPtEtaPhiE(tag.pt, tag.eta, tag.phi, tag.energy)
-
-        mass = (pTag + pProbe).M()
-        hDiEleMass.Fill(mass)
-        
-output.cd()
-hDiEleMass.Write()
-output.Write()
-output.Close()
-source.Close()
-'''
