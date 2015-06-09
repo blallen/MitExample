4682623a92f3019ab76e08d9c432e6eda9be73d4
Brandon <ballen@mit.edu>
2015-06-09 12:14:53 -0400

    Added handling for arbitrary tree size and mass range.

--- Fitting/scripts/example.py
+++ Fitting/scripts/example.py
@@ -8,7 +8,7 @@ ROOT.gSystem.Load('libMitExampleFitting.so')
 RooFit = ROOT.RooFit
 mithep = ROOT.mithep
 
-def fitZpeak( mass, name, dataset ):
+def fitZpeak( mass, name ):
     '''Fit mass peak with BreitWigner signal plus Exponential exponential background and extract number of electrons within Z window [80,100]'''
 
     print 'Fitting for '+name
@@ -16,8 +16,8 @@ def fitZpeak( mass, name, dataset ):
     sig = workspace.factory('BreitWigner::sig_'+name+'(mass_'+name+', m0_'+name+'[90.,50.,130.], w_'+name+'[1.0,0.,5.0])')
     bkg = workspace.factory('Exponential::bkg_'+name+'(mass_'+name+', lambda_'+name+'[-1.,-100.,0.])')
 
-    nsig = workspace.factory('nsig_'+name+'[200.,0.,10000.]')
-    nbkg = workspace.factory('nbkg_'+name+'[200.,0.,10000.]')
+    nsig = workspace.factory('nsig_'+name+'[200.,0.,'+str(nEvents)+']')
+    nbkg = workspace.factory('nbkg_'+name+'[200.,0.,'+str(nEvents)+']')
     
     spb = workspace.factory('SUM::spb_'+name+'(nsig_'+name+' * sig_'+name+',nbkg_'+name+' * bkg_'+name+')')
 
@@ -32,7 +32,7 @@ def fitZpeak( mass, name, dataset ):
     canvas.SaveAs('DiEleMass_'+name+'.png')
 
     mass.setRange('Zpeak_'+name, 80., 100.)
-    mass.setRange('all_'+name,0.,200.)
+    mass.setRange('all_'+name,0.,maxmass)
     tmp = ROOT.RooArgSet(mass)
     fReal = sig.createIntegral( tmp, 'Zpeak_'+name).getVal() / sig.createIntegral( tmp, 'all_'+name).getVal()
     nReal = fReal * ( nsig.getVal())
@@ -47,17 +47,20 @@ def fitZpeak( mass, name, dataset ):
 
 source = ROOT.TFile.Open('ntuples_noid_skim.root')
 tree = source.Get('events')
-        
+nEvents = tree.GetEntries()
+print 'Total number of events: '+str(nEvents)
+maxmass = 200.
+
 # Yutaro code
 workspace = ROOT.RooWorkspace('workspace')
-mass_noID = workspace.factory('mass_noID[0.,200.]')
-mass_passID = workspace.factory('mass_passID[0.,200.]')
+mass_noID = workspace.factory('mass_noID[0.,'+str(maxmass)+']')
+mass_passID = workspace.factory('mass_passID[0.,'+str(maxmass)+']')
 argset = ROOT.RooArgSet(mass_noID, mass_passID)
 
 dataset = mithep.FittingUtils.createDataSet(tree, argset)
 
-nNoID = fitZpeak( mass_noID, 'noID', dataset)
-nPassID = fitZpeak( mass_passID, 'passID', dataset)
+nNoID = fitZpeak( mass_noID, 'noID')
+nPassID = fitZpeak( mass_passID, 'passID')
 
 eff = nPassID / nNoID
 
